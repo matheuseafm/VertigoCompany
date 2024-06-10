eecc68ac050378801b801fca2d4a6c19a7ec1fab
Matheus <101060126+matheuseafm@users.noreply.github.com>
2024-06-10 15:53:59 -0300

    Add files via upload
    v1.0

---
+++ Vertigo Company/Carro.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Carro {
+    private String modelo;
+    private String placa;
+    private ArrayList<Multa> multas;
+
+    public Carro(String modelo, String placa) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.multas = new ArrayList<Multa>();
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public ArrayList<Multa> getMultas() {
+        return multas;
+    }
+
+    public void addMulta(Multa multa) {
+        multas.add(multa);
+    }
+}
---
+++ Vertigo Company/Multa.java
@@ -0,0 +1,9 @@
+class Multa {
+    String infracao;
+    double valor;
+
+    public Multa(String infracao, double valor) {
+        this.infracao = infracao;
+        this.valor = valor;
+    }
+}
